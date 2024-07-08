110b914ff6bea61ede1490f222d53145dedf1d54
bmorgulis <116917489+bmorgulis@users.noreply.github.com>
2024-07-08 11:01:02 -0400

    Add files via upload

---
+++ Website/footer.php
@@ -0,0 +1,7 @@
+<HR />
+<P> &copy; 2024 - Binyomin Zev Morgulis & William Feller</P>
+<P><a href="contactus.php">Contact Us</a></P>
+any other footer information
+
+</BODY>
+</HTML>
---
+++ Website/header.php
@@ -0,0 +1,9 @@
+<HTML>
+<HEAD>
+<LINK rel="stylesheet" href="styles.css" type="text/css" />
+</HEAD>
+<BODY>
+<P>THE LOGO</P>
+<?php include "menu.php"; ?>
+<HR />
+
