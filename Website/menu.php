ad3590aef5c8c583b27ddccb36c98d4c1a4e17c9
bmorgulis <116917489+bmorgulis@users.noreply.github.com>
2024-07-08 23:35:54 -0400

    chore: Update menu links in menu.php

--- Website/menu.php
+++ Website/menu.php
@@ -2,15 +2,15 @@
 <P>THE MENU</P>
 <NAV>   
     <ul class="nav-menu">
-        <li><a href="page1.php">Home</a></li>
-        <li><a href="page2.php">About</a></li>
-        <li><a href="page3.php">Products</a></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="products.php">Products</a></li>
             <li class="dropdown">
-                <a href="#">Hi</a>
+                <a href="#">Products</a>
                 <ul class='dropdown-content'>
-                    <li><a href="page2.php">dd1</a></li>
-                    <li><a href="page2.php">dd2</a></li>
-                    <li><a href="page2.php">dd3</a></li>
+                    <li><a href="small.php">Small</a></li>
+                    <li><a href="medium.php">Medium</a></li>
+                    <li><a href="large.php">Large</a></li>
                 </ul>
             </li>
             <li><a href="contactus.php">Contact Us</A></LI>
