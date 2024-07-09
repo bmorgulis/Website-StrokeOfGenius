ad94e4ed058c50299ac0087eddc961945e35ff1e
william-feller <116769998+zevythegreat@users.noreply.github.com>
2024-07-09 00:59:34 -0400

    chore: Update main page content

--- Website/index.php
+++ Website/index.php
@@ -1,6 +1,7 @@
 <?php include "header.php"; ?>
 
-MAIN PAGE INFORMATION - This web site is.....
+Since our debut in 2024, Stroke of Genius has had one thing in mind: to provide our customers with high-quality, tasteful, and beautiful art that would looks great
+in the studio, and in your sitting room. We aim to provide a superior shopping experience, and we are confident that you will be pleased with our products.
 
 
 <?php include "footer.php"; ?>
