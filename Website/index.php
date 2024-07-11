eccc41a583f74f8afd5bf94c30832e58701db612
bmorgulis <116917489+bmorgulis@users.noreply.github.com>
2024-07-10 23:21:03 -0400

    Update main page layout and content. Add background image again. need to change it at some point.

--- Website/index.php
+++ Website/index.php
@@ -1,4 +1,5 @@
 <?php include "header.php"; ?>
+<div id="full-main">
 <h1 id="main-page">
     
 Since our debut in 2024, Stroke of Genius has had one thing in mind: to provide 
@@ -6,7 +7,7 @@ our customers with high-quality, tasteful, and beautiful art that would look
 great in the studio, and in your sitting room. We aim to provide a superior 
 shopping experience, and we are confident that you will be pleased with our products.
 </h1>
-
+</div>
 <?php include "footer.php"; ?>
 
 
