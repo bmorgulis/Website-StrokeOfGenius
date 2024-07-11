9f0759203c9efc8effc41123f3b951fd9c7ad786
bmorgulis <116917489+bmorgulis@users.noreply.github.com>
2024-07-11 11:39:02 -0400

    Refactor login functionality and improve login page layout

--- Website/Login.php
+++ Website/Login.php
@@ -9,25 +9,6 @@
     ?>
 <body class="login-container">
     <h2>Login</h2>
-    <!-- <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $username = $_POST["username"];
-            $password = $_POST["password"];
-            if ($username == "bzm" && $password == "bzm") {
-                $_SESSION["username"] = $username;
-                // if (session_is_registered("username")) {
-                //     echo "<p>Welcome, $username!</p>";
-                // }
-                $session_encode = session_encode();//gets the session data and encodes it
-                $filehandle = fopen("C:\Users\bzmor\Class Code\Website\Website-StrokeOfGenius\Website\sessions\sessions.txt", "w");//opens the file for writing
-                fwrite($filehandle, $session_data);//writes the session data to the file    
-                fclose($filehandle);//closes the file
-                header("Location: products.php");//redirects to the products page
-            } else {
-                echo "<p>Invalid username and/or password.</p>";
-            }
-        }
-        ?> -->
     <form method="POST" action="response.php">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required><br><br>
