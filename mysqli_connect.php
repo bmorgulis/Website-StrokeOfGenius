fbb4b1cb2ffcb5e73ff99e0da051068b51d4fd2f
bmorgulis <116917489+bmorgulis@users.noreply.github.com>
2024-07-22 10:48:12 -0400

    Refactor mysqli_connect.php to use a constant for the database server

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,10 +1,10 @@
 <?php
+const DB_SERVER = 'localhost';
 const DB_USERNAME = 'root';
 const DB_PASSWORD = '';
 const DB_HOST = 'localhost';
 const DB_NAME = 'StrokeOfGeniusDB';
 
-$dbc = @mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME)
+$dbc = @mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME)
 or die('Could not connect to MySQL: ' . mysqli_connect_error());
 
-    
