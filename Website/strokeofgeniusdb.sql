73399fb630c5afef3d0b6fea5939bd51b69ddbf2
bmorgulis <116917489+bmorgulis@users.noreply.github.com>
2024-07-21 22:51:10 -0400

    chore: Refactor database schema for strokeofgeniusdb, improve login functionality and page layout, and update session handling

--- Website/strokeofgeniusdb.sql
+++ Website/strokeofgeniusdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2024 at 05:27 AM
+-- Generation Time: Jul 22, 2024 at 04:50 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,11 +28,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `accountinfo` (
+  `userid` int(255) NOT NULL,
   `fname` varchar(25) NOT NULL,
   `lname` varchar(25) NOT NULL,
   `email` varchar(25) NOT NULL,
   `address` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `accountinfo`
+--
+
+INSERT INTO `accountinfo` (`userid`, `fname`, `lname`, `email`, `address`) VALUES
+(1, 'binyomin', 'morgulis', 'bzm@gmail.com', '7601 147th street'),
+(2, 'william', 'feller', 'wfeller@gmail.com', '4321 45th ave');
 
 -- --------------------------------------------------------
 
@@ -41,6 +50,7 @@ CREATE TABLE `accountinfo` (
 --
 
 CREATE TABLE `authorizedusers` (
+  `userid` varchar(255) NOT NULL,
   `username` varchar(25) NOT NULL,
   `password` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -49,11 +59,22 @@ CREATE TABLE `authorizedusers` (
 -- Dumping data for table `authorizedusers`
 --
 
-INSERT INTO `authorizedusers` (`username`, `password`) VALUES
-('binyomin', 'morgulis'),
-('william', 'feller'),
-('bzm', 'bzm'),
-('admin', 'hello');
+INSERT INTO `authorizedusers` (`userid`, `username`, `password`) VALUES
+('1', 'binyomin', 'morgulis'),
+('2', 'william', 'feller'),
+('', 'bzm', 'bzm'),
+('', 'admin', 'hello');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `userid` int(255) NOT NULL,
+  `items` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -70,18 +91,6 @@ CREATE TABLE `categories` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `logininfo`
---
-
-CREATE TABLE `logininfo` (
-  `username` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `role` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `sortby`
 --
 
@@ -89,6 +98,16 @@ CREATE TABLE `sortby` (
   `price` varchar(30) NOT NULL,
   `name` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accountinfo`
+--
+ALTER TABLE `accountinfo`
+  ADD PRIMARY KEY (`userid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
