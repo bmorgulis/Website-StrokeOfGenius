9c2f56a3b3168c573afdeee29a51f821736170c9
bmorgulis <116917489+bmorgulis@users.noreply.github.com>
2024-07-23 11:44:03 -0400

    Refactor PHPMailer integration, improve form submission and database insertion

---
+++ Website/sendEmail.php
@@ -0,0 +1,43 @@
+<!-- mail2.php is connected to this page -->
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+
+if(isset($_POST['name']) && isset($_POST['email'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $body = $_POST['body'];
+
+    require_once "PHPMailer/PHPMailer.php";
+    require_once "PHPMailer/SMTP.php";
+    require_once "PHPMailer/Exception.php";
+
+    $mail = new PHPMailer();
+
+    //SMTP Settings
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "strokeofgeniuswebsite@gmail.com";
+    $mail->Password = "qiifmhjyqoglglon"; //Enter your password
+    $mail->Port = 465; //587
+    $mail->SMTPSecure = "ssl"; //tls
+
+    //Email Settings
+    $mail->isHTML(true);
+    $mail->setFrom($email, $name);
+    $mail->addAddress("strokeofgeniuswebsite@gmail.com");
+    $mail->Subject = "$email ($subject)";
+    $mail->Body = $body;
+
+    if ($mail->send()) {
+        $status = "success";
+        $response = "Email is sent!";
+    } else {
+        $status = "failed";
+        $response = "Something is wrong: <br>" . $mail->ErrorInfo;
+    }
+
+    exit(json_encode(array("status" => $status, "response" => $response)));
+
+}
