cb7b743a50491d899dd712374ac1a27f9123a70a
bmorgulis <116917489+bmorgulis@users.noreply.github.com>
2024-07-24 17:27:10 -0400

    Refactor title text animation on homepage

--- Website/titletext.js
+++ Website/titletext.js
@@ -1,18 +1,27 @@
-const textEl = document.getElementById('titletext')
-const text = 'Stroke of Genius!'
-let idx = 1
-let speed = 300
+const textEl = document.getElementById('titletext');
+const text = 'Stroke of Genius!';
+let idx = 1;
+let speed = 275;
 
-writeText()
+writeText();
 
 function writeText() {
-    textEl.innerText = text.slice(0, idx)
+    textEl.innerText = text.slice(0, idx);
 
-    idx++
+    idx++;
 
-    if(idx > text.length) {
-        idx = 1
+    if (idx > text.length) {
+        let blinkInterval = setInterval(() => {
+            textEl.style.visibility = textEl.style.visibility === 'visible' ? 'hidden' : 'visible';
+        }, 400); // Toggle visibility every 500ms to create a blinking effect
+
+        setTimeout(() => { // Wait for 3 seconds before restarting
+            clearInterval(blinkInterval); // Stop blinking
+            textEl.style.visibility = 'visible'; // Ensure text is visible before restarting
+            idx = 1;
+            writeText();
+        }, 3500); 
+    } else {
+        setTimeout(writeText, speed);
     }
-
-    setTimeout(writeText, speed)
 }
